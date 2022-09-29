3b71837f7a65e51bf4249ee0aee3454c50efb5b6
reiters190295 <reiters190295@sus.htl-grieskirchen.at>
2022-09-29 10:40:36 +0200

    Beispiel2 Code der Berechnungen

--- 2Hausuebung/src/at/reiters190295/bsp3/Main.java
+++ 2Hausuebung/src/at/reiters190295/bsp3/Main.java
@@ -7,6 +7,26 @@ public class Main
     public static void main(String[] args)
     {
         NumberTester nt = new NumberTester("numbers.txt");
-
+        nt.setOddEvenTester(number -> (number%2) == 0);
+        nt.setPrimeTester(number -> {
+            boolean flag = false;
+            for (int i = 2; i <= number / 2; ++i) {
+                if (number % i == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+            return flag;
+        });
+        nt.setPalindromeTester(number -> {
+            int reverse = 0;
+            int num = number;
+            while (number != 0) {
+                int remainder = num % 10;
+                reverse = reverse * 10 + remainder;
+                num = num / 10;
+            }
+            return number == reverse;
+        });
     }
 }
--- 2Hausuebung/src/at/reiters190295/bsp3/NumberTester.java
+++ 2Hausuebung/src/at/reiters190295/bsp3/NumberTester.java
@@ -1,15 +1,23 @@
 package at.reiters190295.bsp3;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class NumberTester
 {
     private NumberTest oddTester;
     private NumberTest primeTester;
     private NumberTest palindromeTester;
 
+    private String fileName;
+
 
     public NumberTester(String fileName)
     {
-
+        this.fileName = fileName;
+        testFile();
     }
 
     public void setOddEvenTester(NumberTest oddTester)
@@ -28,6 +36,45 @@ public class NumberTester
     }
 
     public void testFile()
+    {
+        try
+        {
+            Scanner scanner = new Scanner(new FileInputStream(fileName));
+            int i = Integer.parseInt(scanner.nextLine());
+            for(int j = 0; j < i; j++)
+            {
+                String line = scanner.nextLine();
+                String[] split = line.split(" ");
+                switch(Integer.parseInt(split[0]))
+                {
+                    case 1:
+                        break;
+
+                    case 2:
+                        break;
+
+                    case 3:
+                        palindromeTester.testNumber(Integer.parseInt(split[1]))
+                        break;
+                }
+            }
+        }
+        catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean isEven(int number)
+    {
+        return
+    }
+
+    private boolean isPrime(int number)
+    {
+
+    }
+
+    private boolean isPalindrome(int number)
     {
 
     }
