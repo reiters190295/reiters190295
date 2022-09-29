c46b70e91292e8827e9758599454af8d8342e141
reiters190295 <reiters190295@sus.htl-grieskirchen.at>
2022-09-29 10:01:38 +0200

    Beispiel2 Datei numbers.csv erstellt

--- 2Hausuebung/src/at/reiters190295/bsp3/Main.java
+++ 2Hausuebung/src/at/reiters190295/bsp3/Main.java
@@ -1,6 +1,12 @@
 package at.reiters190295.bsp3;
 
+import at.reiters190295.bsp2.HalloJavamitForEach;
+
 public class Main
 {
-
+    public static void main(String[] args)
+    {
+        NumberTester nt = new NumberTester("numbers.csv");
+        
+    }
 }
