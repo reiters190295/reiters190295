b41f7f80f1e85d85bafbc2814359f4b68f4bc996
reiters190295 <reiters190295@sus.htl-grieskirchen.at>
2022-10-05 22:19:36 +0200

    Beispiel3 Benutzerausgabe und Anzeige

---
+++ 2022-09-29_NumberTester-Calculator/2Hausuebung/src/at/reiters190295/bsp4/DoubleNumber.java
@@ -0,0 +1,20 @@
+package at.reiters190295.bsp4;
+
+public class DoubleNumber
+{
+    private Number first;
+    private Number second;
+
+    public DoubleNumber(Number first, Number second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public Number getFirst() {
+        return first;
+    }
+
+    public Number getSecond() {
+        return second;
+    }
+}
--- 2022-09-29_NumberTester-Calculator/2Hausuebung/src/at/reiters190295/bsp4/Main.java
+++ 2022-09-29_NumberTester-Calculator/2Hausuebung/src/at/reiters190295/bsp4/Main.java
@@ -1,67 +1,123 @@
 package at.reiters190295.bsp4;
 
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main
 {
     private Scanner scanner = new Scanner(System.in, "Windows-1252");
 
+    private AbstractCalculator ratio;
+    private AbstractCalculator vector;
+    private AbstractCalculator complex;
+
+    private Number num1;
+    private Number num2;
+
     public static void main(String[] args)
     {
         Main main = new Main();
-
+        main.init();
 
         while(true)
         {
-            switch(main.showUserInterface())
+            int calculator = main.showUserInterface();
+            if(calculator == 4)
             {
-                case 1:
+                return;
+            }
+            DoubleNumber dn = main.showNumber();
+            int type = main.showCalcTypes();
 
-
-                    break;
-
-                case 2:
-                    main.showVector();
-                    main.showCalcTypes();
-                    break;
-
-                case 3:
-
-                    break;
-
-                default: return;
+            switch (calculator)
+            {
+                case 1: main.calcRatio(calculator, dn, type);break;
+                case 2: main.calcVector(calculator, dn, type);break;
+                case 3: main.calcComplex(calculator, dn,type);break;
             }
         }
     }
 
+    private void calcRatio(int calculator, DoubleNumber dn, int type)
+    {
+        switch (type)
+        {
+            case 1: showSolution(ratio.add(dn.getFirst(), dn.getSecond()), calculator);break;
+            case 2: showSolution(ratio.subtract(dn.getFirst(), dn.getSecond()), calculator);break;
+            case 3: showSolution(ratio.multiply(dn.getFirst(), dn.getSecond()), calculator);break;
+            case 4: showSolution(ratio.divide(dn.getFirst(), dn.getSecond()), calculator);break;
+        }
+    }
+
+    private void calcVector(int calculator, DoubleNumber dn, int type)
+    {
+        switch (type)
+        {
+            case 1: showSolution(vector.add(dn.getFirst(), dn.getSecond()), calculator);break;
+            case 2: showSolution(vector.subtract(dn.getFirst(), dn.getSecond()), calculator);break;
+            case 3: showSolution(vector.multiply(dn.getFirst(), dn.getSecond()), calculator);break;
+            case 4: showSolution(vector.divide(dn.getFirst(), dn.getSecond()), calculator);break;
+        }
+    }
+
+    private void calcComplex(int calculator, DoubleNumber dn, int type)
+    {
+        switch (type)
+        {
+            case 1: showSolution(complex.add(dn.getFirst(), dn.getSecond()), calculator);break;
+            case 2: showSolution(complex.subtract(dn.getFirst(), dn.getSecond()), calculator);break;
+            case 3: showSolution(complex.multiply(dn.getFirst(), dn.getSecond()), calculator);break;
+            case 4: showSolution(complex.divide(dn.getFirst(), dn.getSecond()), calculator);break;
+        }
+    }
+
+    private double kuerzen(double a, double b)
+    {
+        double result = a % b;
+        if(result == 0)
+            return b;
+        return kuerzen(b, result);
+    }
+
     private void init()
     {
-        AbstractCalculator ratio = new RationalCalculator(
+        ratio = new RationalCalculator(
                 (x, y) ->
                 {
                     double xZ = x.getA() * y.getB();
                     double yZ = y.getA() * x.getB();
                     double n = x.getB() * y.getB();
-                    return new Number(xZ+yZ, n);
+                    xZ = xZ+yZ;
+                    double kurz = kuerzen(xZ, n);
+                    return new Number(xZ/kurz, n/kurz);
                 },
                 (x, y) ->
                 {
                     double xZ = x.getA() * y.getB();
                     double yZ = y.getA() * x.getB();
                     double n = x.getB() * y.getB();
-                    return new Number(xZ-yZ, n);
+                    xZ = xZ-yZ;
+                    double kurz = kuerzen(xZ, n);
+                    return new Number(xZ/kurz, n/kurz);
                 },
                 (x, y) ->
                 {
-                    return new Number(x.getA()*y.getA(), x.getB()*y.getB());
+                    double a = x.getA()*y.getA();
+                    double b = x.getB()*y.getB();
+                    double kurz = kuerzen(a, b);
+                    return new Number(a/kurz, b/kurz);
                 },
                 (x, y) ->
                 {
-                    return new Number(x.getA()*y.getB(), x.getB()*y.getA());
+                    double a = x.getA()*y.getB();
+                    double b = x.getB()*y.getA();
+                    double kurz = kuerzen(a, b);
+                    return new Number(a/kurz, b/kurz);
                 }
         );
 
-        AbstractCalculator vector = new VectorCalculator(
+        vector = new VectorCalculator(
                 (x, y) ->
                 {
                     return new Number(x.getA()+y.getA(), x.getB()+y.getB());
@@ -78,13 +134,12 @@ public class Main
                 },
                 (x, y) ->
                 {
-                    double a = x.getA() * x.getB();
-                    double b = y.getA() * y.getB();
-                    return new Number(a, b);
+                    double a = x.getA() * y.getA() + x.getB() * y.getB();
+                    return new Number(a, 0);
                 }
         );
 
-        AbstractCalculator complex = new ComplexCalculator(
+        complex = new ComplexCalculator(
                 (x, y) ->
                 {
                     return new Number(x.getA()+y.getA(), x.getB()+y.getB());
@@ -109,9 +164,7 @@ public class Main
 
     private Number mult(Number x, Number y)
     {
-        double a = x.getA() * y.getB();
-        double b = y.getA() * x.getB();
-        return new Number(x.getA() * y.getA() + x.getB() * y.getB(), x.getA() * y.getB() + x.getB() * y.getA());
+        return new Number(x.getA() * y.getA() - x.getB() * y.getB(), x.getA() * y.getB() + x.getB() * y.getA());
     }
 
     private int showUserInterface()
@@ -143,7 +196,7 @@ public class Main
             catch(NumberFormatException ex)
             {
                 System.err.println();
-                System.err.println("Please choose a number between 1-4!");
+                System.err.println("Please use numbers!");
                 System.err.println();
                 System.err.flush();
             }
@@ -151,12 +204,30 @@ public class Main
         }
     }
 
-    private void showSolution()
+    private void showSolution(Number result, int calculatior)
     {
-
+        System.out.printf("---------------------------\nSolution\n");
+        switch (calculatior)
+        {
+            case 1:
+                System.out.println(result.getA() + "\n-----\n" + result.getB()); break;
+            case 2:
+                System.out.println("("+ result.getA() + ")\n(" + result.getB() + ")"); break;
+            case 3:
+                if(result.getB() >= 0)
+                {
+                    System.out.println(result.getA() + " + " + result.getB() + "i");
+                }
+                else
+                {
+                    System.out.println(result.getA() + " - " + -result.getB() + "i");
+                }
+                break;
+        }
+        System.out.println("\n");
     }
 
-    private void showCalcTypes()
+    private int showCalcTypes()
     {
         while(true)
         {
@@ -171,6 +242,7 @@ public class Main
                 System.out.println();
                 if(i < 1 || i > 4)
                 {
+                    System.out.flush();
                     System.err.println();
                     System.err.println("Please choose a number between 1-4!");
                     System.err.println();
@@ -178,13 +250,14 @@ public class Main
                 }
                 else
                 {
-                    return;
+                    return i;
                 }
             }
             catch(NumberFormatException ex)
             {
+                System.out.flush();
                 System.err.println();
-                System.err.println("Please choose a number between 1-4!");
+                System.err.println("Please use numbers!");
                 System.err.println();
                 System.err.flush();
             }
@@ -192,28 +265,34 @@ public class Main
         }
     }
 
-    private void showVector()
+    private DoubleNumber showNumber()
     {
         int xa; int xb; int ya; int yb;
-        try
+        while (true)
         {
-            System.out.print("Enter number x a> ");
-            xa = Integer.parseInt(scanner.nextLine());
-            System.out.print("\nEnter number x b> ");
-            xb = Integer.parseInt(scanner.nextLine());
-            System.out.print("\nEnter number y a> ");
-            ya = Integer.parseInt(scanner.nextLine());
-            System.out.print("\nEnter number y b> ");
-            yb = Integer.parseInt(scanner.nextLine());
-            System.out.println();
-            return;
+            try
+            {
+                System.out.print("Enter number x a> ");
+                xa = Integer.parseInt(scanner.nextLine());
+                System.out.print("Enter number x b> ");
+                xb = Integer.parseInt(scanner.nextLine());
+                System.out.print("Enter number y a> ");
+                ya = Integer.parseInt(scanner.nextLine());
+                System.out.print("Enter number y b> ");
+                yb = Integer.parseInt(scanner.nextLine());
+                System.out.println();
+                return new DoubleNumber(new Number(xa, xb), new Number(ya, yb));
+            }
+            catch(NumberFormatException ex)
+            {
+                System.out.flush();
+                System.err.println();
+                System.err.println("Please use numbers!");
+                System.err.println();
+                System.err.flush();
+            }
         }
-        catch(NumberFormatException ex)
-        {
-            System.err.println();
-            System.err.println("Please use numbers!");
-            System.err.println();
-            System.err.flush();
-        }
+
+
     }
 }
